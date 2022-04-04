fbb20e341144550bc2534bfc35a41cbd3fb8bd32
Tang Quoc Thai <victortang212@gmail.com>
2022-04-04 19:56:49 +0700

    Count primes

---
+++ count_primes.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+# https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        nums = [0, 0] + [1] * (n - 2)
+        for i in range(2, int(sqrt(n) + 1)):
+            if nums[i] == 1:
+                for j in range(i * i, n, i):
+                    nums[j] = 0
+        return sum(nums)
+
+
+if __name__ == "__main__":
+    Solution.countPrimes(object, n=20)
