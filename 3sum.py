f226590c8d2905f0f9d38723038c6722a8da07e8
Tang Quoc Thai <victortang212@gmail.com>
2022-04-04 20:36:07 +0700

    3sum using 2sum

---
+++ 3sum.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        def twoSum(nums: List[int], target: int) -> List[List[int]]:
+            supplements = set()
+            results = []
+            for num in nums:
+                if target - num not in supplements:
+                    supplements.add(num)
+                else:
+                    results.append([num, target - num])
+            return results
+
+        nums = sorted(nums)
+        results = set()
+
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+
+            supplements = None
+            for j in range(len(nums) - 1, i, -1):
+                if nums[j] + nums[j - 1] >= -nums[i]:
+                    supplements = nums[i + 1 : j + 1]
+                    break
+
+            if supplements:
+                pairs = twoSum(nums=supplements, target=-nums[i])
+                if len(pairs) > 0:
+                    for pair in pairs:
+                        if (nums[i], pair[0], pair[1]) not in results:
+                            results.add((nums[i], pair[0], pair[1]))
+        return [list(r) for r in list(results)]
+
+
+if __name__ == "__main__":
+    Solution.threeSum(object, nums=[0, 0, 0])
