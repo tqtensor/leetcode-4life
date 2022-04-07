441ad2c9c0283b356c56b497b1162f0d993baa3e
Tang Quoc Thai <victortang212@gmail.com>
2022-04-07 08:22:24 +0700

    Add explanation for No of subseq

--- number_of_subsequences.py
+++ number_of_subsequences.py
@@ -7,15 +7,16 @@ class Solution:
         i, j = 0, len(nums) - 1
 
         res = 0
-        NUM = 10 ** 9 + 7
+        NUM = 10**9 + 7
         while i <= j:
             if nums[i] + nums[j] > target:
                 j -= 1
             elif nums[i] + nums[j] <= target:
-                print(pow(2, j - i, NUM))
+                # For each position inside the range (i, j),
+                # we have two choices to select or not select
+                # an element, so 2**n possibilities
                 res += pow(2, j - i, NUM)
                 i += 1
-
         return res % NUM
 
 
