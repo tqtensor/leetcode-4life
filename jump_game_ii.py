bf466a14f80351a267a1c0bb8fe10a2a3efec263
TangQuocThai <thaitq2@vng.com.vn>
2022-07-03 12:12:45 +0700

    Add Jump game II

--- jump_game_ii.py
+++ jump_game_ii.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution(object):
     def jump(self, nums: List[int]) -> int:
 
--- longest_increasing_subseq.py
+++ longest_increasing_subseq.py
@@ -6,7 +6,8 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(n):
-            # Here we pass three arg's, which means find the position of nums[i] in the nums array within index i.
+            # Here we pass three arg's, which means find the position
+            # of nums[i] in the nums array within index i.
             idx = bisect_left(nums, nums[i], hi=i)
             if idx != i:
                 nums[idx], nums[i] = nums[i], float("inf")
